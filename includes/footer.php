efc4f3325eb40f76b3ac5ab30b9cb119104171db
Aryhermansyah <aryhermansyah29@gmail.com>
2025-05-01 09:05:55 +0700

    Add includes/footer.php 1746065154712

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+</main>
+    <footer class="bg-white py-4 text-center text-gray-600 text-sm mt-8">
+        <p>&copy; <?php echo date('Y'); ?> Wedding Organizer. All rights reserved.</p>
+    </footer>
+</body>
+</html>
